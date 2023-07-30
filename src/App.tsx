4a17c10f2bd909c885d33782bf4c79c21f9ad658
Bryann Torres <arleyro91@gmail.com>
2023-07-30 14:31:10 -0500

    Added one constellation card

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 import "aframe";
 import { useEffect, useState } from "react";
 
+interface ConstellationInfo {
+  name: string;
+  position: {
+    x: number;
+    y: number;
+    z: number;
+  };
+  description: string;
+}
+
 function App() {
   const [pauseMenuOpen, setPauseMenuOpen] = useState(false);
+
+  const constellationsInfo: ConstellationInfo[] = [
+    {
+      name: "Aries",
+      position: {
+        x: -9,
+        y: 5,
+        z: 8,
+      },
+      description: "Aries is a constellation in the northern sky.",
+    },
+  ];
 
   window.addEventListener("keydown", (e) => {
     if (e.key === "Escape") {
       setPauseMenuOpen(!pauseMenuOpen);
     }
   });
+
+  const calcOverflowPosition = (position: number) => {
+    if (position === 0) return position + 0.1;
+    return position + Math.sign(position) * 0.1;
+  };
 
   useEffect(() => {
     const pauseMenu = document.getElementById("pauseMenu");
@@ -36,10 +63,66 @@ function App() {
             src="/images/constellation_figures.png"
             alt="figures"
           />
+          <img id="aries_img" src="/images/aries.jpg" alt="aries_img" />
         </a-assets>
         <a-sky src="#starmap" radius="100" />
         <a-sky src="#boundaries" radius="99" transparent />
         <a-sky src="#figures" radius="99" transparent />
+
+        {constellationsInfo.map((constellation) => {
+          const rotationY =
+            Math.atan2(constellation.position.x, constellation.position.z) *
+            (180 / Math.PI);
+          return (
+            <div
+              key={constellation.name}
+              onClick={() => {
+                console.info("clicked");
+              }}
+            >
+              <a-plane
+                width="2.5"
+                height="1"
+                material="color: gray; opacity: 0.4"
+                position={`${constellation.position.x} ${constellation.position.y} ${constellation.position.z}`}
+                rotation={`0 ${rotationY} 0`}
+              >
+                <a-text
+                  value={constellation.name}
+                  position="0 0 0"
+                  align="center"
+                  width="18"
+                />
+              </a-plane>
+              <a-entity id={constellation.name.toLowerCase()} visible="true">
+                <a-plane
+                  color="#fff"
+                  position={`${calcOverflowPosition(
+                    constellation.position.x
+                  )} ${calcOverflowPosition(
+                    constellation.position.y
+                  )} ${calcOverflowPosition(constellation.position.z)}`}
+                  rotation={`0 ${rotationY} 0`}
+                  width="5"
+                  height="8"
+                >
+                  <a-image
+                    src={`#${constellation.name.toLowerCase()}_img`}
+                    position="0 1.5 0.2"
+                    width="5"
+                    height="5"
+                  />
+                  <a-text
+                    value={constellation.description}
+                    align="center"
+                    color="black"
+                    position="0 -2.5 0.3"
+                  />
+                </a-plane>
+              </a-entity>
+            </div>
+          );
+        })}
 
         <a-camera>
           <a-cursor
